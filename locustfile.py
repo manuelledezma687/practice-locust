11ebc4d72bb48ce5fcb18b4b6789f2e998b249b0
manuelledezma687 <manuelledezma687@gmail.com>
2023-02-05 12:51:11 -0300

    locust practice

--- locustfile.py
+++ locustfile.py
@@ -3,17 +3,17 @@ from locust import HttpUser,task, between
 
 class QuickStart(HttpUser):
     wait_time = between(1,5)
-    
+
     @task
     def hello_world(self):
         self.client.get("/hello")
-    #     self.client.get("/world")
+        self.client.get("/world")
 
-    # @task(3)
-    # def view_items(self):
-    #     for item_id in range(10):
-    #         self.client.get(f"/item?id={item_id}", name="/item")
-    #         time.sleep(1)
+    @task(3)
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
 
-    # def on_start(self):
-    #     self.client.post("/login", json={"username":"standard_user", "password":"secret_sauce"})
+    def on_start(self):
+        self.client.post("/login", json={"username":"standard_user", "password":"secret_sauce"})
